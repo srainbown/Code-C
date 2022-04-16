b8d1d225efc3d5f88aefebf2786fa59f11f52792
紫川秀 <tszy111@126.com>
2022-04-17 02:54:40 +0800

    Initial Commit

---
+++ C1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C1
+//
+//  Created by 紫川秀 on 2022/4/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
